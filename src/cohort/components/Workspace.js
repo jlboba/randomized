87f10b82c4d31df50ef119b128537b55afe21267
Jerrica Bobadilla <>
2018-11-23 12:50:05 -0800

    more workspace nav styling

--- src/cohort/components/Workspace.js
+++ src/cohort/components/Workspace.js
@@ -1,24 +1,44 @@
 // ==============================
 // DEPENDENCIES
 // ==============================
-import React from 'react'
+import React, { Component } from 'react'
 
 // ==============================
-// ROSTER COMPONENT
+// WORKSPACE COMPONENT
 // ==============================
-const Workspace = () => {
-  return (
-    <div className="workspace-container">
-      <div className="workspace-navigation">
-        <ul>
-          <li><span class="lnr lnr-user"></span> add student</li>
-          <li><span class="lnr lnr-file-empty"></span> whiteboard</li>
-          <li><span class="lnr lnr-dice"></span> randomizer</li>
-          <li><span class="lnr lnr-list"></span> view all lists</li>
-        </ul>
+class Workspace extends Component {
+  // STATE
+  state = {
+    activeSpace: 'add'
+  }
+
+  // HANDLER METHODS
+  handleActiveSpace = (space) => {
+    console.log('hi');
+    this.setState(prevState => {
+      return {
+        activeSpace: space
+      }
+    })
+    console.log(this.state);
+  }
+
+  // RENDER
+  render() {
+    return (
+      <div className="workspace-container">
+        <div className="workspace-navigation">
+          <ul>
+            <li className={this.state.activeSpace === 'add' ? 'active-nav' : null} onClick={() => this.handleActiveSpace('add')}><span className="lnr lnr-user"></span> add student</li>
+            <li className={this.state.activeSpace === 'whiteboard' ? 'active-nav' : null} onClick={() => this.handleActiveSpace('whiteboard')}><span className="lnr lnr-file-empty"></span> whiteboard</li>
+            <li className={this.state.activeSpace === 'randomizer' ? 'active-nav' : null} onClick={() => this.handleActiveSpace('randomizer')}><span className="lnr lnr-dice"></span> randomizer</li>
+            <li className={this.state.activeSpace === 'lists' ? 'active-nav' : null} onClick={() => this.handleActiveSpace('lists')}><span className="lnr lnr-list"></span> view all lists</li>
+          </ul>
+        </div>
+        when moon shine it's your time
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 // ==============================
