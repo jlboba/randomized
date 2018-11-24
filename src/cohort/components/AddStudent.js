68163d85c291c396950f9c98a62636c2be00337c
Jerrica Bobadilla <>
2018-11-23 20:58:25 -0800

    add student form styling

--- src/cohort/components/AddStudent.js
+++ src/cohort/components/AddStudent.js
@@ -11,14 +11,12 @@ class AddStudent extends Component {
   // AXIOS CALLS
   addNewStudent = (e) => {
     e.preventDefault()
-    console.log(this.props);
     axios.post('https://randomized-api.herokuapp.com/students', {
       name: this.refs.name.value,
       nickname: this.refs.nickname.value,
       cohort_id: this.props.cohortId
     })
       .then((createdStudent) => {
-        console.log(createdStudent);
         this.props.getCohort()
       })
       .catch(err => console.log(err))
@@ -27,7 +25,8 @@ class AddStudent extends Component {
   // RENDER
   render() {
     return (
-      <div>
+      <div className="form-container">
+        <h1>ADD A STUDENT</h1>
         <form onSubmit={this.addNewStudent}>
           <input type="text" ref="name" placeholder="name"/>
           <input type="text" ref="nickname" placeholder="nickname"/>
