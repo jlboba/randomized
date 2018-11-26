7b53bf9f69c957c3e0931e6ef91738592413fd1e
Jerrica Bobadilla <>
2018-11-25 22:27:53 -0800

    whiteboard save list toggle button

--- src/cohort/components/Whiteboard.js
+++ src/cohort/components/Whiteboard.js
@@ -14,6 +14,7 @@ import Category from './Category'
 class Whiteboard extends Component {
   // STATE
   state = {
+    showCreateList: false,
     categories: []
   }
 
@@ -51,6 +52,15 @@ class Whiteboard extends Component {
     })
   }
 
+  toggleCreateList = () => {
+    this.setState(prevState => {
+      return {
+        showCreateList: !prevState.showCreateList
+      }
+    })
+  }
+
+  // SUBMIT METHODS
   handleCategorySubmit = (e) => {
     // prevent default form
     e.preventDefault()
@@ -66,6 +76,11 @@ class Whiteboard extends Component {
     this.updateCategoryState(categoryName)
   }
 
+  handleListSubmit = (e) => {
+    e.preventDefault()
+    console.log('nothing for now hi')
+  }
+
   // LIFE CYCLES
   componentDidMount() {
     this.props.getCohort()
@@ -79,7 +94,7 @@ class Whiteboard extends Component {
       <div className="whiteboard-container">
         {/* ======== FORM TO ADD CATEGORY ======== */}
         <form onSubmit={this.handleCategorySubmit}>
-          <input type="text" ref="categoryName" placeholder="name"/>
+          <input type="text" ref="categoryName" placeholder="name your category"/>
           <input type="submit" value="add category"/>
         </form>
         {/* ======== CATEGORIES ======== */}
@@ -95,6 +110,15 @@ class Whiteboard extends Component {
             )
           })}
         </div>
+        {/* ======== BUTTON TO TOGGLE SAVING AS A LIST ======== */}
+        <button onClick={this.toggleCreateList}>SAVE AS LIST?</button>
+        {/* ======== FORM TO SAVE AS LIST ======== */}
+        {this.state.showCreateList ?
+          <form onSubmit={this.handleListSubmit}>
+            <input type="text" ref="listName" placeholder="name your list"/>
+            <input type="submit" value="create list"/>
+          </form> : null
+        }
       </div>
     )
   }
